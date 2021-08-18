962a67577564a194fc062c9c485a917689932564
ZwerOxotnik <zweroxotnik@gmail.com>
2021-08-18 20:25:19 +0300

    Update to 0.9.4

--- compiler.lua
+++ compiler.lua
@@ -283,9 +283,10 @@ local function eval( ast, control, memory, modules, program_counter, clock )
         return {signal = signalID, count = value}
     end
     local setout = function( value )
-        local params = control.parameters
+        local params = table.deepcopy(control.parameters) -- it worked before without this
         params.first_constant = value.count
         params.output_signal = value.signal
+		control.parameters = params
     end
     -- Multiplex Helper Functions
     local function getregister( index_expr )
